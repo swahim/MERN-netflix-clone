3a251105b6b3b3f3ade42bd10e68b6f45a81b68c
Swahim Namdev <swahimn@gmail.com>
2021-11-10 12:22:45 +0530

    completed user routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,31 +1,110 @@
-const User = require('../models/User');
-const CryptoJS = require('crypto-js');
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 class Controller {
-    //UPDATE
-    async update(req, res, next) {
-        if(req.params.id === req.user.id || req.user.isAdmin){
-            if(req.body.password){
-                req.body.password = CryptoJS.AES.encrypt(
-                    password,
-                    process.env.SECRET_KEY
-                  ).toString();
-            }
-            try{
-                const user = await User.findByIdAndUpdate(req.params.id, {$set : req.body},
-                    {new : true});
-                res.status(200).json(user);
-            }catch(err){
-                res.status(500).json(err);
-                next(err);
-            }
-        }else{
-            res.status(403).json("You cant update account");
-        }
+  //UPDATE
+  async update(req, res, next) {
+    if (req.params.id === req.user.id || req.user.isAdmin) {
+      if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+          password,
+          process.env.SECRET_KEY
+        ).toString();
+      }
+      try {
+        const user = await User.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body },
+          { new: true }
+        );
+        res.status(200).json(user);
+      } catch (err) {
+        res.status(500).json(err);
+        next(err);
+      }
+    } else {
+      res.status(403).json("You cant update account");
     }
-    //DELETE
-    //GET
-    //GET ALL
-    //GET ALL USER STATS
+  }
+  //DELETE
+  async delete(req, res, next) {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "User deleted successfully" });
+      } catch (err) {
+        res.status(500).json(err);
+        next();
+      }
+    } else {
+      res.status(403).json("You cant delete this account");
+    }
+  }
+  //GET USER
+  async getUser(req, res, next) {
+    try {
+      const user = await User.findById(req.params.id);
+      !user && res.status(401).json({ message: "User not found" });
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json({ err });
+      next(err);
+    }
+  }
+  //GET ALL
+  async getAllUser(req, res, next) {
+    try {
+      const query = req.query.new;
+      const userArray = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      !userArray && res.status(401).json({ message: "Users not found" });
+      res.status(200).json(userArray);
+    } catch (err) {
+      res.status(500).json(err);
+      next();
+    }
+  }
+  //GET ALL USER STATS
+  async getUserStats(req, res, next) {
+      try {
+        const date = new Date();
+        const lastYear = date.setFullYear(date.setFullYear() - 1);
+        const month = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+      const data = await User.aggregate([
+        {
+          $project: 
+          {
+              month : {
+                  $month : "$createdAt"
+              },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json(err);
+      next();
+    }
+  }
 }
 
 module.exports = new Controller();
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,5 +3,9 @@ const { verifyToken } = require("../middlewares/VerifyToken");
 const router = require('express').Router();
 
 router.put('/update/:id',verifyToken ,Controller.update);
+router.delete('/delete/:id', verifyToken ,Controller.delete);
+router.get('/find/:id' ,Controller.getUser);
+router.get('/', Controller.getAllUser);
+router.get('/stats', Controller.getUserStats);
 
 module.exports = router;
