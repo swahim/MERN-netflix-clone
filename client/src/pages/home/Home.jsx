7d34c388309322298b5e7ac3c67de3ba300f161a
Swahim Namdev <swahimn@gmail.com>
2021-11-13 11:50:16 +0530

    first client side commit

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,12 +2,26 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.scss";
 import List from "../../components/list/List";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({type}) => {
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const getRandomList = async () => {
+      try{
+        const res = await axios.get
+      }catch(err){
+        console.log(err);
+      }
+    }
+  })
+
   return (
     <div className="home">
       <Navbar />
-      <Featured/>
+      <Featured type={type}/>
       <List/>
       <List/>
       <List/>
