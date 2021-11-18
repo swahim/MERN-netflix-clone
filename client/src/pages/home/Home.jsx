ed2df11763e56683d7b6f8c8709843ce685f8258
Swahim Namdev <swahimn@gmail.com>
2021-11-18 16:45:56 +0530

    added admin feature

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,27 +5,35 @@ import List from "../../components/list/List";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const Home = ({type}) => {
+const Home = ({ type }) => {
   const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
 
   useEffect(() => {
     const getRandomList = async () => {
-      try{
-        const res = await axios.get
-      }catch(err){
+      try {
+        const res = await axios.get(
+          `/list/getlist${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`
+        );
+        setLists(res.data);
+        console.log(lists);
+      } catch (err) {
         console.log(err);
       }
-    }
-  })
+    };
+    getRandomList();
+  }, [type, genre]);
 
   return (
     <div className="home">
       <Navbar />
-      <Featured type={type}/>
-      <List/>
-      <List/>
-      <List/>
-      <List/>
+      <Featured type={type} />(
+      {lists.map((list) => (
+        <List list={list}/>
+      ))}
+      )
     </div>
   );
 };
--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,13 +1,18 @@
 import { ArrowBackOutlined } from "@material-ui/icons";
 import "./watch.scss";
+import {useLocation, Link} from 'react-router-dom'; 
 
 export default function Watch() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="watch">
+      <Link to={"/"}>
       <div className="back">
         <ArrowBackOutlined />
         Home
       </div>
+      </Link>
       <video
         className="video"
         autoPlay
